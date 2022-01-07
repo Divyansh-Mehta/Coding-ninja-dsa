60410b184a7c6262746078d64f01de3d6b1ef02a
Divyansh-Mehta <divyanshmehta01@gmail.com>
2022-01-07 17:49:03 +0530

    7th jan 2022, 5:48pm

--- Data-Structures-in-C++/Lecture-19-DP-2/Code/loot-house.cpp
+++ Data-Structures-in-C++/Lecture-19-DP-2/Code/loot-house.cpp
@@ -82,18 +82,16 @@ int maxMoneyLooted(int *arr, int n)
 //DP
 int maxMoneyLooted(int *arr, int n)
 {
-    if (n == 0)
-    {
-        return 0;
-    }
     if (n == 1)
     {
         return arr[0];
     }
     int *dp = new int[n];
     dp[0] = arr[0];
-    dp[1] = max(arr[0], arr[1]);
-
+    if (n > 1)
+    {
+        dp[1] = max(arr[0], arr[1]);
+    }
     for (int i = 2; i < n; i++)
     {
         dp[i] = max(dp[i - 2] + arr[i], dp[i - 1]);
