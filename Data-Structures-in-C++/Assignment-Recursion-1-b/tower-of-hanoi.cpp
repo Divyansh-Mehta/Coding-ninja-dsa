c5ab2c3108cc8f8cdcd8eb388c745977fa3d48e4
Divyansh Mehta <72566559+Divyansh-Mehta@users.noreply.github.com>
2022-07-16 20:34:36 +0530

    improved

--- Data-Structures-in-C++/Assignment-Recursion-1-b/tower-of-hanoi.cpp
+++ Data-Structures-in-C++/Assignment-Recursion-1-b/tower-of-hanoi.cpp
@@ -39,15 +39,11 @@ a c
 #include <iostream>
 using namespace std;
 
+// Time: - O(2^n) Space: - O(2^n)
 void towerOfHanoi(int n, char source, char auxiliary, char destination)
 {
     if (n == 0)
         return;
-    if (n == 1)
-    {
-        cout << source << " " << destination << endl;
-        return;
-    }
 
     towerOfHanoi(n - 1, source, destination, auxiliary);
     cout << source << " " << destination << endl;
