5af68b2e1bab8ff8848cf31606ea03cef0e69fa9
Virat-Gupta <99909273+Virat-Gupta@users.noreply.github.com>
2023-07-07 01:47:55 +0530

    Update search-in-tries.cpp
    changed question to comment

--- Data-Structures-in-C++/Lecture-16-Tries-and-Huffman-Coding/Code/search-in-tries.cpp
+++ Data-Structures-in-C++/Lecture-16-Tries-and-Huffman-Coding/Code/search-in-tries.cpp
@@ -1,9 +1,10 @@
+/*
 Code : Search in Tries
 
 Implement the function Search for the Trie class.
 For a Trie, write the function for searching a word. Return true if the word is found successfully, otherwise return false.
 Note : main function is given for your reference which we are using internally to test the code.
-
+*/
 
 #include <iostream>
 #include <string>
